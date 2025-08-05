6c109be150e32ffc63a958c7bd4f462a2f4c3700
alunoUFU <alunoufu@ufu.br>
2025-08-05 08:34:50 -0300

    refactor

--- src/main.c
+++ src/main.c
@@ -1,26 +1,48 @@
 #include <math.h>
 #include "wokwi-api.h"
+
 #define PI 3.141592653589793
+#define NUM_SINE 3
 
-static float freqs[5];
-static float amplitude;
+typedef struct {
+  pin_t vout;
+  timer_t tid;
+  float freqs[NUM_SINE];
+  float amps[NUM_SINE];
+} opamp_chip_t;
 
-void chip_init(void) {
-  freqs[0] = attr_read_float(attr_init_float("freq1", 10.0f));
-  freqs[1] = attr_read_float(attr_init_float("freq2", 110.0f));
-  freqs[2] = attr_read_float(attr_init_float("freq3", 0.0f));
-  freqs[3] = attr_read_float(attr_init_float("freq4", 0.0f));
-  freqs[4] = attr_read_float(attr_init_float("freq5", 0.0f));
-  amplitude = attr_read_float(attr_init_float("amplitude", 1.0f));
-}
-
-void chip_tick(void) {
+void chip_tick(void *user_data) {
+  opamp_chip_t *chip = (opamp_chip_t *)user_data;
   double t = get_sim_nanos() * 1e-9;
   double y = 0.0;
-  for (int i = 0; i < 5; i++) {
-    if (freqs[i] > 0.0f) {
-      y += sin(2.0 * PI * freqs[i] * t);
+
+  for (int i = 0; i < NUM_SINE; i++) {
+    if (chip->freqs[i] > 0.0f && chip->amps[i] != 0.0f) {
+      y += chip->amps[i] * sin(2.0 * PI * chip->freqs[i] * t);
     }
   }
-  pin_dac_write(pin_init("OUT", ANALOG), (float)(y * amplitude));
+
+  pin_dac_write(chip->vout, (float)y);
+}
+
+void chip_init(void) {
+  opamp_chip_t *chip = malloc(sizeof(opamp_chip_t));
+
+  chip->freqs[0] = attr_read_float(attr_init_float("freq1", 10.0f));
+  chip->freqs[1] = attr_read_float(attr_init_float("freq2", 110.0f));
+  chip->freqs[2] = attr_read_float(attr_init_float("freq3", 0.0f));
+
+  chip->amps[0] = attr_read_float(attr_init_float("amp1", 1.0f));
+  chip->amps[1] = attr_read_float(attr_init_float("amp2", 1.0f));
+  chip->amps[2] = attr_read_float(attr_init_float("amp3", 1.0f));
+
+  chip->vout = pin_init("OUT", ANALOG);
+
+  const timer_config_t tcfg = {
+    .callback = chip_tick,
+    .user_data = chip,
+  };
+
+  chip->tid = timer_init(&tcfg);
+  timer_start(chip->tid, 1000, true);  // 1µs = 1 MHz update rate
 }
